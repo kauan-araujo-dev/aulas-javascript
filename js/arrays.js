b43e14972865463f478657168eba50b64dd76eb5
kauan-araujo-dev <kauankaka2007@gmail.com>
2025-07-30 17:26:22 -0300

    Testando arrays

--- js/arrays.js
+++ js/arrays.js
@@ -1,1 +1,12 @@
-"use strict"
+"use strict";
+
+// Declarando um array (vetor)
+let linguagens = ["HTML", "CSS", "JS", "PYTHON", "JAVA"];
+
+//Acessando a estrutura do array inteiro:
+console.log(linguagens)
+
+// Acessando um determinado elemento através do índice
+console.log(linguagens[0])
+
+console.log(`O ${linguagens[0]} é uma linguagem de marcação`)
