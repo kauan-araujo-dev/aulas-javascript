dcdf7ffb2282063b77abfdcb215e3deb76b4a383
kauan-araujo-dev <kauankaka2007@gmail.com>
2025-07-30 16:41:22 -0300

    Exercício e aspas dentro de strings

---
+++ js/02-meu-script.js
@@ -0,0 +1,26 @@
+/* Exercícios */
+// 1) Crie uma variável chamada "uc" e coloque nela o número da UC que estamos
+
+// 2) Crie uma variável chamada "competencia" e coloque nela uma frase que represente o que estamos estudando nesta UC.
+
+// 3) Monte e mostre uma frase no console exibindo a UC e a competência. Exemplo: Estamos na UC2 estudando Front-End com JavaScript.
+
+// 1)
+let uc = 2;
+
+// 2) 
+let competencia = "Logica de programação com JavaScript";
+
+console.log(`Estamos na UC${uc} estudando ${competencia}.`)
+
+// ERRADO!
+// console.log("Gosto "muito" de CSS");
+
+// CORRETO! Aspas simples como comando, e duplas como texto
+console.log('Gosto "muito" de CSS');
+
+// CORRETO! Aspas duplas como comando, e simples como texto
+console.log("Gosto 'muito' de CSS");
+
+// CORRETO! Escapando as aspas como texto
+console.log("Gosto \"muito\" de CSS");
