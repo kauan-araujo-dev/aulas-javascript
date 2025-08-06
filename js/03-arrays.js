d99da9284e2bd9d9bb6abd0c1280c26d34737469
kauan-araujo-dev <kauankaka2007@gmail.com>
2025-08-06 14:33:54 -0300

    Ponto e vírgula no final das senteças e Arrays com dados heterogêneos

--- js/03-arrays.js
+++ js/03-arrays.js
@@ -4,9 +4,13 @@
 let linguagens = ["HTML", "CSS", "JS", "PYTHON", "JAVA"];
 
 //Acessando a estrutura do array inteiro:
-console.log(linguagens)
+console.log(linguagens);
 
 // Acessando um determinado elemento através do índice
-console.log(linguagens[0])
+console.log(linguagens[0]);
 
-console.log(`O ${linguagens[0]} é uma linguagem de marcação`)
+console.log(`O ${linguagens[0]} é uma linguagem de marcação`);
+let seiLa = 6;
+const coisas = [10, "Senac", "<h2>", seiLa, 15.88];
+console.log(coisas[1]);
+console.log(coisas[4]);
