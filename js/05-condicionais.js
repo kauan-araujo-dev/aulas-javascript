c9d1dd7b26ba56e8b3f9905cf5813f4f8f69cfa1
kauan-araujo-dev <kauankaka2007@gmail.com>
2025-08-19 15:15:00 -0300

    Exercício com estruturas de decisão

--- js/05-condicionais.js
+++ js/05-condicionais.js
@@ -17,6 +17,24 @@ if(idade < 18){
     console.log("Maior de idade");
 }
 
-let maiorDeidade = idade < 18 ? "Menor de idade" : "Maior de idade";
+// 1. Crie duas variáveis conforme a seguir:
+ 
+// - Nota 1 (contendo um valor de 0 a 10)
+// - Nota 2 (contendo outro valor de 0 a 10)
+ 
+// 2. Crie uma variável chamada "Média" que receberá o valor CALCULADO da média das duas notas informadas. 
+// DICA: você deve SOMAR as duas notas e DEPOIS dividir por 2.
+ 
+// 3. Programe uma condicional que verifique o valor da média calculada. Se a média for maior/igual a 7, mostre "aprovado". 
+// Caso contrário, mostre "reprovado".
 
-console.log(maiorDeidade)
+let nota1 = 10;
+let nota2 = 4;
+
+let media = (nota1 + nota2) / 2;
+
+if(media >= 7){
+    console.log("Aprovado")
+}else{
+    console.log("Reprovado")
+}
