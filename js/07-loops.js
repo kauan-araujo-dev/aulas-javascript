1eaf362afb2f174ec1a9f25dc7b11a499c85d5aa
kauan-araujo-dev <kauankaka2007@gmail.com>
2025-08-20 14:59:23 -0300

    Loop for

--- js/07-loops.js
+++ js/07-loops.js
@@ -16,3 +16,21 @@ while(i <= 5){
     console.log("Valor do contador: " + i);
     i++;
 }
+
+console.log("\nExemplo 2: FOR(para)");
+
+for(let j = 1; j <= 10; j++){
+    console.log("J: " + j)
+}
+
+for(let k = 10; k > 1; k --){
+    console.log("K vale" + k)
+}
+
+console.log("Exemplo 4: loop e array");
+
+const bandas = ["Slayer", "Metallica", "Iron Maiden", "Nightwish", "Judas Priest"];
+
+for(let i = 0; i < bandas.length; i++){
+    console.log(bandas[i])
+}
