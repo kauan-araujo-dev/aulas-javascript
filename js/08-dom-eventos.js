a2ee149451db303128678a9aba4d0b10c8baea85
kauan-araujo-dev <kauankaka2007@gmail.com>
2025-08-27 13:47:58 -0300

    selecionando elementos do html

--- js/08-dom-eventos.js
+++ js/08-dom-eventos.js
@@ -1,2 +1,13 @@
 "use strict";
 
+/** Exemplo 1 */
+
+// Procurando (querySelector) 
+const exemplo01 = document.querySelector("#exemplo01");
+const mensagem01 = document.querySelector("#mensagem01");
+const pagina = document.querySelector("body")
+console.log(exemplo01);
+console.log(mensagem01);
+console.log(pagina);
+
+
