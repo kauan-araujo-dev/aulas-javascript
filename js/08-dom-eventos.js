0cdd2c4b2da9a3f3b33561d47d55f98f8f73134f
kauan-araujo-dev <kauankaka2007@gmail.com>
2025-08-27 14:47:30 -0300

    eventos em js

--- js/08-dom-eventos.js
+++ js/08-dom-eventos.js
@@ -5,7 +5,7 @@
 // Procurando (querySelector) 
 const exemplo01 = document.querySelector("#exemplo01");
 const mensagem01 = document.querySelector("#mensagem01");
-const pagina = document.querySelector("body")
+const pagina = document.querySelector("body");
 console.log(exemplo01);
 console.log(mensagem01);
 console.log(pagina);
@@ -14,6 +14,24 @@ console.log(pagina);
 O resultado é uma espécie de array de elementos (NodeList) */
 const paragrafros = document.querySelectorAll("p");
 
-console.log(paragrafros)
+console.log(paragrafros);
 
+/** Sobre Eventos
+ * Um evento é um acontecimento ou fênomeno que, quando acontece, dispara ações em nosso programa/site/aplicação.
+ * 
+ * O JavaScript suporta centenas de tipos de evento. Exemplos: clique do mouse, pressionar teclas, tocar na tela, rolagem da página
+ */
+
+// Exemplo detectando eventos
+
+
+exemplo01.addEventListener("click", function(){
+    mensagem01.textContent = "h2 acionado";
+    mensagem01.classList.add("destaque");
+})
+
+mensagem01.addEventListener("dblclick", function (){
+    mensagem01.textContent = "";
+    mensagem01.classList.remove("destaque");
+})
 
